773ec8028a0e1c6244473316770eca347cee578a
tvOdyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-07-27 16:23:41 -0400

    update; add new modules

--- exordium-desktop/src/app/app.module.ts
+++ exordium-desktop/src/app/app.module.ts
@@ -2,9 +2,17 @@ import 'reflect-metadata';
 import '../polyfills';
 
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
+
+import { RecaptchaModule, RecaptchaFormsModule, RECAPTCHA_SETTINGS, RecaptchaSettings } from 'ng-recaptcha';
+import { ToastrModule } from 'ngx-toastr';
+import { NotyfToast } from './shared/components/notyf.toast';
+import { LoadingBarModule } from '@ngx-loading-bar/core';
+
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 
@@ -19,6 +27,7 @@ import { AuthSigninComponent } from './_pages/auth/auth-signin/auth-signin.compo
 import { DashboardComponent } from './_pages/dashboard/dashboard/dashboard.component';
 
 import { AppComponent } from './app.component';
+import { PerfectScrollbarDirective } from './shared/directives/scrollbar/perfect-scrollbar.directive';
 
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
@@ -28,17 +37,28 @@ export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
 @NgModule({
   declarations: [
     AppComponent,
+    PerfectScrollbarDirective,
     AuthSigninComponent, 
     DashboardComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
+
+    RecaptchaModule, 
+    RecaptchaFormsModule,
+    ToastrModule.forRoot({
+      toastComponent: NotyfToast,
+      timeOut: 6000,
+      positionClass: 'toast-bottom-right'
+    }),
+    LoadingBarModule,
+
     CoreModule,
     SharedModule,
-
-
 
     AppRoutingModule,
     TranslateModule.forRoot({
@@ -49,7 +69,21 @@ export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
       }
     })
   ],
-  providers: [],
+  entryComponents: [
+    //NotyfToast
+  ],
+  providers: [
+    {
+      provide: LocationStrategy,
+      useClass: HashLocationStrategy,
+   },
+    {
+      provide: RECAPTCHA_SETTINGS,
+      useValue: {
+        siteKey: '6LeB5uIUAAAAAMQWnwCUpUHbdsHO4iV4emdn9KOL'
+      } as RecaptchaSettings,
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
