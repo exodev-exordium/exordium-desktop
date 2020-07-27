a0e0b67e88f9c02f61d44682e2e656f65b056960
tvOdyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-07-27 16:25:26 -0400

    update; add modules to the shared module (not really working tho?)

--- exordium-desktop/src/app/shared/shared.module.ts
+++ exordium-desktop/src/app/shared/shared.module.ts
@@ -3,13 +3,27 @@ import { CommonModule } from '@angular/common';
 
 import { TranslateModule } from '@ngx-translate/core';
 
+import { NgSelectModule } from '@ng-select/ng-select';
+
 import { PageNotFoundComponent } from './components/';
 import { WebviewDirective } from './directives/';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [PageNotFoundComponent, WebviewDirective],
-  imports: [CommonModule, TranslateModule, FormsModule],
-  exports: [TranslateModule, WebviewDirective, FormsModule]
+  imports: [
+    CommonModule, 
+    TranslateModule,
+
+    NgSelectModule,
+  
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  exports: [
+    TranslateModule, 
+    WebviewDirective, 
+    FormsModule
+  ]
 })
 export class SharedModule {}
