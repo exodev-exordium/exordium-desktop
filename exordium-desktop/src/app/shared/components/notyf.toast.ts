299f02b174dc648e73918b32529fd5518bbc8695
FearGannicus <43388217+FearGannicus@users.noreply.github.com>
2020-08-03 07:18:20 -0400

    update; notyf-toast

--- exordium-desktop/src/app/shared/components/notyf.toast.ts
+++ exordium-desktop/src/app/shared/components/notyf.toast.ts
@@ -1,91 +1,91 @@
 import {
-    animate,
-    keyframes,
-    state,
-    style,
-    transition,
-    trigger,
-  } from '@angular/animations';
-  import { Component } from '@angular/core';
-  import { Toast, ToastrService, ToastPackage } from 'ngx-toastr';
-  
-  @Component({
-    selector: 'notyf-toast-component',
-    styles: [],
-    template: `
-      <div class="notyf__toast notyf__toast">
-        <div class="notyf__wrapper">
-          <div class="notyf__icon">
-            <i class="notyf__icon--placeholder"></i>
-          </div>
-          <div class="notyf__message">{{ title }} {{ message }}</div>
+  animate,
+  keyframes,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
+import { Component } from '@angular/core';
+import { Toast, ToastrService, ToastPackage } from 'ngx-toastr';
+
+@Component({
+  selector: 'notyf-toast-component',
+  styles: [],
+  template: `
+    <div class="notyf__toast notyf__toast">
+      <div class="notyf__wrapper">
+        <div class="notyf__icon">
+          <i class="notyf__icon--placeholder"></i>
         </div>
-        <div class="notyf__ripple"></div>
+        <div class="notyf__message">{{ title }} {{ message }}</div>
       </div>
-    `,
-    animations: [
-      trigger('flyInOut', [
-        state('inactive', style({ opacity: 0 })),
-        transition(
-          'inactive => active',
-          animate(
-            '300ms ease-out',
-            keyframes([
-              style({
-                opacity: 0,
-                bottom: '-15px',
-                'max-height': 0,
-                'max-width': 0,
-                'margin-top': 0,
-              }),
-              style({
-                opacity: 0.8,
-                bottom: '-3px',
-              }),
-              style({
-                opacity: 1,
-                bottom: '0',
-                'max-height': '200px',
-                'margin-top': '12px',
-                'max-width': '400px',
-              }),
-            ]),
-          ),
+      <div class="notyf__ripple"></div>
+    </div>
+  `,
+  animations: [
+    trigger('flyInOut', [
+      state('inactive', style({ opacity: 0 })),
+      transition(
+        'inactive => active',
+        animate(
+          '300ms ease-out',
+          keyframes([
+            style({
+              opacity: 0,
+              bottom: '-15px',
+              'max-height': 0,
+              'max-width': 0,
+              'margin-top': 0,
+            }),
+            style({
+              opacity: 0.8,
+              bottom: '-3px',
+            }),
+            style({
+              opacity: 1,
+              bottom: '0',
+              'max-height': '200px',
+              'margin-top': '12px',
+              'max-width': '400px',
+            }),
+          ]),
         ),
-        state(
-          'active',
-          style({
-            bottom: '0',
-            'max-height': '200px',
-            'margin-top': '12px',
-            'max-width': '400px',
-          }),
+      ),
+      state(
+        'active',
+        style({
+          bottom: '0',
+          'max-height': '200px',
+          'margin-top': '12px',
+          'max-width': '400px',
+        }),
+      ),
+      transition(
+        'active => removed',
+        animate(
+          '300ms ease-out',
+          keyframes([
+            style({
+              opacity: 1,
+              transform: 'translateY(0)'
+            }),
+            style({
+              opacity: 0,
+              transform: 'translateY(25%)'
+            }),
+          ]),
         ),
-        transition(
-          'active => removed',
-          animate(
-            '300ms ease-out',
-            keyframes([
-              style({
-                opacity: 1,
-                transform: 'translateY(0)'
-              }),
-              style({
-                opacity: 0,
-                transform: 'translateY(25%)'
-              }),
-            ]),
-          ),
-        ),
-      ]),
-    ],
-  })
-  export class NotyfToast extends Toast {
-    // constructor is only necessary when not using AoT
-    constructor(
-      protected toastrService: ToastrService,
-      public toastPackage: ToastPackage,
-    ) {
-      super(toastrService, toastPackage);
-    }
+      ),
+    ]),
+  ],
+})
+export class NotyfToast extends Toast {
+  // constructor is only necessary when not using AoT
+  constructor(
+    protected toastrService: ToastrService,
+    public toastPackage: ToastPackage,
+  ) {
+    super(toastrService, toastPackage);
   }
+}
